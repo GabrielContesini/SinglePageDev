43cefecd6d1a9f321de6b7a0ab64c6a0869e43cd
GabrielContesini <gabrielct.moraes@gmail.com>
2022-03-08 15:52:40 -0300

    sidebar

---
+++ script.js
@@ -0,0 +1,14 @@
+/*var hamburguer = document.querySelector('.hamburguer');
+
+console.log(hamburguer);
+
+hamburguer.addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu");
+}); */
+
+document.querySelector('.hamburguer').addEventListener('click', ()=> {
+    document.querySelector('.container').classList.toggle('show-menu');
+});
+
+
+
